65280bd8c94e3e945a93c842d65eda796fe8d4ef
George Clam <george.clam@yahoo.com>
2014-02-13 11:22:03 -0700

    Used ruby and html to create a table that showed info (time zone, pop., flag) of 3 major cities.

--- 209-world-cities/db/schema.rb
+++ 209-world-cities/db/schema.rb
@@ -11,9 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140212181643) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "world_cities", force: true do |t|
+    t.string "city"
+    t.string "flag"
+    t.string "time_zone"
+    t.string "pop"
+  end
 
 end
--- 209-world-cities/db/seeds.rb
+++ 209-world-cities/db/seeds.rb
@@ -1,1 +1,5 @@
-# TODO: write this
+WorldCity.create! city: "London, United Kingdom", pop: "8,000,000", time_zone: "UTC+0000", flag:"/images/uk.png"
+
+WorldCity.create! city: "Paris, France", pop: "2,000,000", time_zone: "UTC+0100", flag: "/images/france.png"
+
+WorldCity.create! city: "Shanghai, China", pop: "14,000,000", time_zone: "UTC+0800", flag: "/images/prc.png"
