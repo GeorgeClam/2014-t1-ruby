4c85b85c11f47addba127ab8a5a0c0f8e3c83203
George Clam <george.clam@yahoo.com>
2014-02-19 13:05:11 -0700

    Used post requests to add information to the database and display that information to the user

--- 219-post-requests/controller.rb
+++ 219-post-requests/controller.rb
@@ -4,10 +4,13 @@ get "/" do
   halt erb(:index)
 end
 
-# TODO: add a POST handler for the "/" route
-
 get "/elements/:id" do
   id = params[:id]
   @element = Element.find(id)
   halt erb(:show)
+end
+
+post "/" do
+  Element.create! name: "Oxygen"
+  redirect "/"
 end
