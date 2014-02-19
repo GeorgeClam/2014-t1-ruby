a47ed235890e8a39a196775518bf51b24c772dcf
George Clam <george.clam@yahoo.com>
2014-02-19 08:55:14 -0700

    Added elements through the console.

--- 218-rake-console/controller.rb
+++ 218-rake-console/controller.rb
@@ -15,3 +15,17 @@ get "/cities/:id" do
   @city = WorldCity.find(id)
   halt erb(:show_city)
 end
+
+get "/create-element" do
+  Element.create! name: "Carbon"
+  halt erb(:index)
+end
+
+get "/cities/:id/double-population" do
+  id = params[:id]
+  @city = WorldCity.find(id)
+  @city.population_in_millions = 
+    @city.population_in_millions.*(2)
+  @city.save!
+  halt erb(:show_city)
+end
