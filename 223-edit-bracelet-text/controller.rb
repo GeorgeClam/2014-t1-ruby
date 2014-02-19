b5fd6bc2331e9fcc5b99966d0485899db13d72d7
George Clam <george.clam@yahoo.com>
2014-02-19 14:03:57 -0700

    Inserted a post tag into the controller coded a new request handler added text fields to edit file

--- 223-edit-bracelet-text/controller.rb
+++ 223-edit-bracelet-text/controller.rb
@@ -7,8 +7,14 @@ end
 
 get "/lines/:id" do
   id = params["id"]
-  @line = BraceletLine.find(id)
+  @lines = BraceletLine.find(id)
   halt erb(:edit)
 end
 
-# TODO: write POST handler
+post "/" do
+  @lines = BraceletLine.new
+  @lines.font_size = params["font_size"]
+  @lines.text = params["text"]
+  @lines.save!
+  redirect "/"
+end
