eb8ce7bf6d945b364847bf9b419d1fc0795ef3f7
George Clam <george.clam@yahoo.com>
2014-02-25 11:18:44 -0700

    Added submit tags to the application to update information to the database, or delete a row all together.

--- 232-validate-goats/models/racing_goat.rb
+++ 232-validate-goats/models/racing_goat.rb
@@ -1,2 +1,7 @@
 class RacingGoat < ActiveRecord::Base
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :gender, presence: true
+  validates :best_100m_time, presence: true
+  validates :dietary_preference, presence: true
 end
