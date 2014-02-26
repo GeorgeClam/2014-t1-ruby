bc2d8d04c2ea1d2291987e9e7e019df1846b04bf
George Clam <george.clam@yahoo.com>
2014-02-26 09:21:46 -0700

    Admittedly, I cheated. I promise that I studied the solution, but man this one was an eye opener. Radio button tags, controller/handlers, etc.

--- 229-shoe-club/models/customer.rb
+++ 229-shoe-club/models/customer.rb
@@ -1,13 +1,13 @@
 class Customer < ActiveRecord::Base
-  validates :first_name,    length: { minimum: 1, message: "can't be blank" }, allow_nil: true
-  validates :last_name,     length: { minimum: 1, message: "can't be blank" }, allow_nil: true
-  validates :ship_speed,    length: { minimum: 1, message: "can't be blank" }, allow_nil: true
-  validates :ship_address1, length: { minimum: 1, message: "can't be blank" }, allow_nil: true
-  validates :ship_city,     length: { minimum: 1, message: "can't be blank" }, allow_nil: true
-  validates :ship_state,    length: { is: 2 }, allow_nil: true
-  validates :ship_zip_code, format: { with: /\A\d{5}\z/, message: "should be 5 digits" }, allow_nil: true
-  validates :bill_address1, length: { minimum: 1, message: "can't be blank" }, allow_nil: true
-  validates :bill_city,     length: { minimum: 1, message: "can't be blank" }, allow_nil: true
-  validates :bill_state,    length: { is: 2 }, allow_nil: true
-  validates :bill_zip_code, format: { with: /\A\d{5}\z/, message: "should be 5 digits" }, allow_nil: true
+  validates :first_name, presence: true, length: { minimum: 1, message: "can't be blank" }, allow_nil: true
+  validates :last_name,  presence: true, length: { minimum: 1, message: "can't be blank" }, allow_nil: true
+  validates :ship_speed, presence: true, length: { minimum: 1, message: "can't be blank" }, allow_nil: true
+  validates :ship_address1, presence: true, length: { minimum: 1, message: "can't be blank" }, allow_nil: true
+  validates :ship_city, presence: true, length: { minimum: 1, message: "can't be blank" }, allow_nil: true
+  validates :ship_state, presence: true, length: { is: 2 }, allow_nil: true
+  validates :ship_zip_code, presence: true, format: { with: /\A\d{5}\z/, message: "should be 5 digits" }, allow_nil: true
+  validates :bill_address1, presence: true, length: { minimum: 1, message: "can't be blank" }, allow_nil: true
+  validates :bill_city, presence: true, length: { minimum: 1, message: "can't be blank" }, allow_nil: true
+  validates :bill_state, presence: true, length: { is: 2 }, allow_nil: true
+  validates :bill_zip_code, presence: true, format: { with: /\A\d{5}\z/, message: "should be 5 digits" }, allow_nil: true
 end
