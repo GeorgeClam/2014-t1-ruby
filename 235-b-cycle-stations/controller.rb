1164bef9fad1cc0acda81eb7f5ae3aa82ff17fcc
George Clam <george.clam@yahoo.com>
2014-02-26 12:45:41 -0700

    Changed the css padding so it didn't look so terrible. Primarily wrote code within the controller that organized the table, excluding information that was not needed.

--- 235-b-cycle-stations/controller.rb
+++ 235-b-cycle-stations/controller.rb
@@ -6,26 +6,32 @@ get "/" do
 end
 
 get "/existing" do
+  @stations = Station.where(status: "existing")
   # TODO: Assign to @stations variable
   halt erb(:index)
 end
 
 get "/private" do
+  @stations = Station.where(property_type: "private")
   # TODO: Assign to @stations variable
   halt erb(:index)
 end
 
 get "/zip/:zip_code" do
+  zip_code = params["zip_code"]
+  @stations = Station.where(zip_code: zip_code)
   # TODO: Assign to @stations variable
   halt erb(:index)
 end
 
 get "/street/Broadway" do
+  @stations = Station.where("address1 ilike ?", "%Broadway%")
   # TODO: Assign to @stations variable
   halt erb(:index)
 end
 
 get "/street/17th" do
+  @stations = Station.where("address1 ilike ?", "%17th%")
   # TODO: Assign to @stations variable
   halt erb(:index)
 end
