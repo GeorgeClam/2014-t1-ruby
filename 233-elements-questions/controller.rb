b8ec4278516f2ed7e0f136ed91ae0ed78baf2d1d
George Clam <george.clam@yahoo.com>
2014-02-26 16:32:47 -0700

    Did not complete 9 & 10. I seem to need to work on creating the handlers. BIG part of coding, it seems. Excited to try and tackle it again later.

--- 233-elements-questions/controller.rb
+++ 233-elements-questions/controller.rb
@@ -5,6 +5,8 @@ get "/" do
 end
 
 post "/" do
+  if params[""]
+  @elements = Element.find_by(name: params[""])
   # TODO: fill out the @answer9 or @answer10 variables in response
   # to whatever the user entered.
   halt erb(:answer_to_9_or_10)
