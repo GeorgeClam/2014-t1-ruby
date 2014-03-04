a9369bf53b8439225f4323c3e588bf1e5e9d1cad
George Clam <george.clam@yahoo.com>
2014-03-04 16:45:06 -0700

    Coded the controller and the view pages to work in tandem, as is the very root of coding, to organize a search page for general information for a few pieces of literature.
    - string interpolation
    - get handlers
    - EMBruby

--- 239-book-search/controller.rb
+++ 239-book-search/controller.rb
@@ -4,12 +4,42 @@ get "/" do
   halt erb(:browse)
 end
 
-# TODO: GET handler for routes like /year/2011, /year/2007, etc.
+get "/year/:year" do
+  year = params["year"]
+  @books = Book.where(publication_year: year)
+  @header = "Books from #{year}:"
+  halt erb(:search_results)
+end
 
-# TODO: GET handler for routes like /author/Olsen,
-#       /author/Crockford, etc.
+get "/author/:last_name" do
+  last_name = params["last_name"]
+  @books = Book.where("author ilike ?", "%#{last_name}")
+  @header = "Books by #{last_name}:"
+  halt erb(:search_results)
+end
 
-# TODO: GET handler for routes like /topic/jQuery, /topic/CSS, etc.
+get "/topic/:topic" do
+  topic = params["topic"]
+  @books = Book.where("title ilike ?", "%#{topic}%")
+  @header = "Books about #{topic}:"
+  halt erb(:search_results)
+end
+
+get "/isbn/:isbn" do
+  isbn = params["isbn"]
+  @books = Book.find_by(isbn: isbn)
+  halt erb(:show)
+end
+
+
+
+
+## TODO: GET handler for routes like /year/2011, /year/2007, etc.
+
+## TODO: GET handler for routes like /author/Olsen,
+##      /author/Crockford, etc.
+
+## TODO: GET handler for routes like /topic/jQuery, /topic/CSS, etc.
 
 # TODO: GET handler for routes like /isbn/1449397220, 
 #       /isbn/1449325947, etc.
